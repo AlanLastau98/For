89f80ebda5d3d26b551d1cca77f18f45695cab7c
Alan Lastau <lastaualan@gmail.com>
2021-10-02 19:18:19 -0300

    Aprendiendo For

---
+++ main.js
@@ -0,0 +1,10 @@
+/*let acumular = ``;
+for (let i = 0; i < 10; i++) {
+  acumular = acumular + "alan sos un capo";
+}
+document.write(acumular);
+
+for (let i = 1; i <= 20; i++) {
+  let ingresarNombre = prompt("ingresa tu nombre");
+  alert("turno numero " + i + " nombre: " + ingresarNombre);
+}*/
